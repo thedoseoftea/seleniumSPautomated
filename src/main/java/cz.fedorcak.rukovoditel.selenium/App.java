e27c221937e68bccedc0dc1a64737e756cbd5329
thedoseoftea <jaroslav.fedorcak@yahoo.com>
2019-11-15 11:05:41 +0100

    including all files

---
+++ src/main/java/cz.fedorcak.rukovoditel.selenium/App.java
@@ -0,0 +1,13 @@
+package seleniumSP;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "I wish i was in Reykjavík looking for Swans." );
+    }
+}

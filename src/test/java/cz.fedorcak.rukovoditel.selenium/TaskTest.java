2b7abd62fde9ec7b5b3fc7940dae8689a7a6c313
thedoseoftea <jaroslav.fedorcak@yahoo.com>
2019-11-17 12:16:53 +0100

    TaskTest both tests

--- src/test/java/cz.fedorcak.rukovoditel.selenium/TaskTest.java
+++ src/test/java/cz.fedorcak.rukovoditel.selenium/TaskTest.java
@@ -33,7 +33,7 @@ public class TaskTest {
 
     @After
     public void tearDown() {
-         driver.close();
+         //driver.close();
     }
 
     @Test
@@ -119,6 +119,156 @@ public class TaskTest {
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[contains(text(),'Reset Search')]")));
         WebElement resetSearchSpan2 = driver.findElement(By.xpath("//span[contains(text(),'Reset Search')]"));
         resetSearchSpan2.click();
+    }
+
+    @Test
+    public void shouldFilterTasksCorrectly() {
+        // Given
+        shouldLoginUsingValidCredentials();
+
+
+        // TODO add setting up default filter on existing fedorcak rather than on fedorcak2
+        // TODO whenever "Reset search" shows up, forgotten by a previous user, reset it
+        // When
+        // First we create new project so we can use the default filter
+        WebElement projectsSpan = driver.findElement(By.xpath("//a//span[contains(text(),'Projects')]"));
+        projectsSpan.click();
+        WebElement addProjectButton = driver.findElement(By.xpath("//button[contains(text(),'Add Project')]"));
+        addProjectButton.click();
+        WebDriverWait wait = new WebDriverWait(driver, 15);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//button[contains(text(),'Save')]")));
+
+        WebElement prioritySelectElement = driver.findElement(By.name("fields[156]"));
+        Select prioritySelect = new Select(prioritySelectElement);
+        prioritySelect.selectByVisibleText("High");
+        WebElement nameInput = driver.findElement(By.name("fields[158]"));
+        String uuid = UUID.randomUUID().toString();
+        nameInput.sendKeys("fedorcak2");
+        WebElement saveButton = driver.findElement(By.xpath("//button[contains(text(),'Save')]"));
+        saveButton.click();
+
+        // Create tasks in the newly created project
+        WebElement projectsSpan2 = driver.findElement(By.xpath("//a//span[contains(text(),'Projects')]"));
+        projectsSpan2.click();
+        WebElement searchInput = driver.findElement(By.xpath("//input[@placeholder='Search']"));
+        searchInput.sendKeys("fedorcak2");
+        WebElement magnifyingGlassButton = driver.findElement(By.xpath("//button[@title='Search']//i[@class='fa fa-search']"));
+        magnifyingGlassButton.click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[contains(text(),'Reset Search')]")));
+        WebElement fedorcak2A = driver.findElement(By.xpath("//a[contains(text(),'fedorcak2')]"));
+        fedorcak2A.click();
+
+        createTask("New");
+        createTask("Open");
+        createTask("Waiting");
+        createTask("Done");
+        createTask("Closed");
+        createTask("Paid");
+        createTask("Canceled");
+
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//button[contains(text(),'Add Task')]")));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//table[@class='table table-striped table-bordered table-hover']")));
+        // Testing default filter
+        // Then
+        Assert.assertEquals(driver.findElements( By.xpath("//tr") ).size(),4); // Expect 3 results in the table + 1 row in the header
+
+        // When
+        // Changing the filter
+        WebElement filtersSpan = driver.findElement(By.xpath("//span[@class='filters-preview-box-heading']"));
+        filtersSpan.click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@id='ajax-modal' and @aria-hidden='false']"))); // Wait for modal to show up
+        WebElement openA = driver.findElement(By.xpath("//li[@class='search-choice']//span[contains(text(),'Open')]/following-sibling::a")); // Find search choice whose sibling is a span with text "Open"
+        openA.click();
+        WebElement saveButton2 = driver.findElement(By.xpath("//button[contains(text(),'Save')]"));
+        saveButton2.click();
+        // Then
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//table[@class='table table-striped table-bordered table-hover']")));
+        Assert.assertEquals(driver.findElements( By.xpath("//tr") ).size(),3); // Expect 2 results in the table + 1 row in the header
+        WebElement resultTable = driver.findElement(By.xpath("//table[@class='table table-striped table-bordered table-hover']"));
+        Assert.assertTrue(resultTable.getText().contains("New"));
+        Assert.assertTrue(resultTable.getText().contains("Waiting"));
+        Assert.assertFalse(resultTable.getText().contains("Open"));
+        Assert.assertFalse(resultTable.getText().contains("Done"));
+        Assert.assertFalse(resultTable.getText().contains("Closed"));
+        Assert.assertFalse(resultTable.getText().contains("Paid"));
+        Assert.assertFalse(resultTable.getText().contains("Canceled"));
+
+        // When
+        WebElement deleteFiltersA = driver.findElement(By.xpath("//a[@href]/child::i[@class='fa fa-trash-o' and @title='Remove Filter']"));
+        deleteFiltersA.click();
+        // Then
+        wait.until(ExpectedConditions.invisibilityOfElementLocated(By.xpath("//span[@class='filters-preview-condition-include']")));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//table[@class='table table-striped table-bordered table-hover']")));
+        Assert.assertEquals(driver.findElements( By.xpath("//tr") ).size(),8); // Expect 7 results in the table + 1 row in the header
+        WebElement resultTable2 = driver.findElement(By.xpath("//table[@class='table table-striped table-bordered table-hover']"));
+        Assert.assertTrue(resultTable2.getText().contains("New"));
+        Assert.assertTrue(resultTable2.getText().contains("Waiting"));
+        Assert.assertTrue(resultTable2.getText().contains("Open"));
+        Assert.assertTrue(resultTable2.getText().contains("Done"));
+        Assert.assertTrue(resultTable2.getText().contains("Closed"));
+        Assert.assertTrue(resultTable2.getText().contains("Paid"));
+        Assert.assertTrue(resultTable2.getText().contains("Canceled"));
+
+
+        // Batch delete:
+        WebElement selectAllItemsInput = driver.findElement(By.name("select_all_items"));
+        selectAllItemsInput.click();
+        WebElement withSelected = driver.findElement(By.xpath("//button[@class='btn btn-default dropdown-toggle' and contains(text(),'With Selected')]"));
+        withSelected.click();
+        WebElement deleteSelected = driver.findElement(By.xpath("//a[@class=' link-to-modalbox']//i[@class='fa fa-trash-o']"));
+        deleteSelected.click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@id='ajax-modal' and @aria-hidden='false']")));
+        WebElement deleteButton = driver.findElement(By.xpath("//button[@type='submit' and @class='btn btn-primary btn-primary-modal-action']"));
+        deleteButton.click();
+
+
+        // Cleanup
+        WebElement projectsSpan4 = driver.findElement(By.xpath("//a//span[contains(text(),'Projects')]"));
+        projectsSpan4.click();
+
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[@title='Delete']//i[@class='fa fa-trash-o']")));
+        WebElement trashCanButton = driver.findElement(By.xpath("//a[@title='Delete']//i[@class='fa fa-trash-o']"));
+        trashCanButton.click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//button[contains(text(),'Delete') and @type='submit']")));
+
+        if (driver.findElements( By.id("delete_confirm") ).size() != 0) {
+            WebElement deleteConfirmInput = driver.findElement(By.id("delete_confirm"));
+            deleteConfirmInput.click();
+        }
+
+        WebElement deleteButton2 = driver.findElement(By.xpath("//button[contains(text(),'Delete') and @type='submit']"));
+        deleteButton2.click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[contains(text(),'Reset Search')]")));
+        WebElement resetSearchSpan2 = driver.findElement(By.xpath("//span[contains(text(),'Reset Search')]"));
+        resetSearchSpan2.click();
+    }
+
+    private void createTask(String status) {
+        WebDriverWait wait = new WebDriverWait(driver, 15);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//button[contains(text(),'Add Task')]")));
+        WebElement addTaskButton = driver.findElement(By.xpath("//button[contains(text(),'Add Task')]"));
+        addTaskButton.click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//button[contains(text(),'Save')]")));
+        WebElement nameInput = driver.findElement(By.name("fields[168]"));
+        String uuid = UUID.randomUUID().toString();
+        nameInput.sendKeys("fedj00" + uuid);
+
+        // Status is set to New by default
+        if (!status.equals("New")) {
+            WebElement statusSelectElement = driver.findElement(By.name("fields[169]"));
+            Select statusSelect = new Select(statusSelectElement);
+            statusSelect.selectByVisibleText(status);
+        }
+        // Priority is set to Medium by default
+        WebElement descriptionIframe = driver.findElement(By.xpath("//iframe[@title='Rich Text Editor, fields_172']"));
+        driver.switchTo().frame(descriptionIframe);
+        WebElement body = driver.findElement(By.xpath("//body"));
+        body.click();
+        JavascriptExecutor executor = (JavascriptExecutor)driver;
+        executor.executeScript("arguments[0].innerHTML = '"+ "I wish I was in Reykjavík looking for Swans"+"'", body);
+        driver.switchTo().defaultContent();
+        WebElement saveButton = driver.findElement(By.xpath("//button[contains(text(),'Save')]"));
+        saveButton.click();
     }
 
     public void shouldLoginUsingValidCredentials() {
